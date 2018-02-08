52569b8e7d1be0be24f43baafc281648d31945c1
zhaohaiyang <zhaohaiyang@wisdomgarden.com>
2018-02-08 09:59:01 +0800

    add activity

---
+++ app.activity/src/main/java/zhy/com/acitvities/MainActivity.kt
@@ -0,0 +1,49 @@
+package zhy.com.acitvities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, Test1Activity::class.java)
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, " onStart ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, " onResume ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, " onPause ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, " onStop ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, " onDestroy ")
+    }
+}
---
+++ app.activity/src/main/java/zhy/com/acitvities/Test1Activity.kt
@@ -0,0 +1,38 @@
+package zhy.com.acitvities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class Test1Activity : AppCompatActivity() {
+
+    private val TAG = Test1Activity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test1)
+
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, " onResume ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, " onPause ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, " onStop ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, " onDestroy ")
+    }
+}
